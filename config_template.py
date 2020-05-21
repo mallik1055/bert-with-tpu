1c9244476fd1b4374fe402ba0ab8f71aaf97a3ff
Mallikarjuna Rao <mallik.1231@gmail.com>
2020-05-21 18:48:22 -0400

    Comments for config file

--- config_template.py
+++ config_template.py
@@ -1,24 +1,24 @@
 
-#GCLOUD CONFIG
+#GCLOUD BUCKET CONFIG
 CLOUD_BUCKET = {
     'name':'524-bert-bucket',
-    'bert_model_path':'uncased_L-12_H-768_A-12', #dont add / in front of the path
-    'raw_text_path':'input_data',
-    'emb_path':'output_data',
+    'bert_model_path':'uncased_L-12_H-768_A-12', #from the root dir but dont add / in front 
+    'raw_text_path':'input_data', #dir with text data to be processed
+    'emb_path':'output_data', #dir where the output(bert embeddings) from the TPU will be stored
     
 }
-
+#GCLOUD TPU CONFIG
 TPU = {
-    'name':'tpu-vm',
-    'ip':'10.240.1.2',
-    'bert_home_dir':'/home/mbudida/bert/'
+    'name':'tpu-vm', #name of the TPU. Check it under https://console.cloud.google.com/compute/tpus
+    'ip':'10.240.1.2', #Internal IP of the TPU. Check it under under https://console.cloud.google.com/compute/tpus
+    'bert_home_dir':'/home/mbudida/bert/' #The directory in gcp VM where you cloned https://github.com/google-research/bert
 }
 
 #Features extractor
-#specify index of the column in the csv
+#specify names and indexes of the columns you want to retain from the input csv to your output JSON file
 EXTRACT_FEATURES = {
-    'raw_text':2,#the main text to be processed
-    'extra_feat':{
+    'raw_text':2, #the main text to be processed
+    'extra_feat':{  #other features along with the embeddings you want to retain in the output JSON file
         'message_id':0,
         'user_id':1,
         'create_at':3,
@@ -29,7 +29,7 @@ EXTRACT_FEATURES = {
 
 #INPUT files containing tweets/text to be processed
 INPUT_FILE = {
-    'in_hdfs':1,
+    'in_hdfs':1, # 0 for local FS
     'path':'/hadoop_data/cm_english_filtered/part7/tweets.10pct.2016.part7.en.30/*',
     'chunk_start_index':5000000,
     'chunk_size':50000,
@@ -39,7 +39,7 @@ INPUT_FILE = {
 
 #OUTPUT files containing BERT emb
 OUTPUT_FILE = {
-    'in_hdfs':1,
+    'in_hdfs':1, #Do you want to put it in HDFS
     'path':'/user/mbudida/cm_english_filtered/bert_output/'
 }
 
